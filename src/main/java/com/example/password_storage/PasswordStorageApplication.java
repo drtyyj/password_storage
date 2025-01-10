275a6b25960151cd5049a95792d4b6d51c32b8a0
MartinWiesinger <76799564+lgbSalzburg@users.noreply.github.com>
2025-01-10 16:33:32 +0100

    added CORS policy for localhost

--- src/main/java/com/example/password_storage/PasswordStorageApplication.java
+++ src/main/java/com/example/password_storage/PasswordStorageApplication.java
@@ -3,6 +3,9 @@ package com.example.password_storage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PasswordStorageApplication {
@@ -10,5 +13,14 @@ public class PasswordStorageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PasswordStorageApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
